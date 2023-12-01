73b7ce86bca633b56e70f522c14291a2cdcc32d9
Turmunkh <turmunkhn@nestsolutions.llc>
2023-11-30 16:10:00 -0800

    Fixed the website

--- app/components/second-section.tsx
+++ app/components/second-section.tsx
@@ -1,69 +1,43 @@
-import Image from "next/image"
-import { TitleAndDescription } from "./title-and-description"
+import Image from "next/image";
+import { TitleAndDescription } from "./title-and-description";
 
-export const FirstSection = () => {
-    return (
-        <div style={{
-            display: "flex",
-            justifyContent: "space-between",
-            backgroundColor: "#F5F7FA",
-            padding: "100px 150px",
-            fontSize: "48px",
-            gap: "20px"
-
-
-
-        }}>
-            <div style={{ padding: "100px 130px", gap: "30px" }}>
-                <TitleAndDescription
-                    h1label="Your Hub for teamwork"
-                    h2label="Give everyone you work with—inside and outside your company—a more productive way to stay in sync. Respond faster with emoji, keep conversations focused in channels, and simplify all your communication into one place."
-                />
-            </div>
-            <div style={{
-                display: "flex", justifyContent: "flex-end",
-                backgroundColor: "#F5F7FA",
-                alignItems: "center",
-                fontSize: "48px",
-            }}
-            >
-
-                <Image style={{
-                    border: "1px",
-                }}
-
-                    src="/first-pic.png"
-                    width={500}
-                    height={500}
-
-                    alt=""
-                />
-                <Image style={{
-                    position: "absolute",
-                    top: "960px",
-                    left: "850px",
-                }
-                }
-                    src="/event 1.png"
-                    width={263}
-                    height={130}
-                    alt=""
-                />
-                <Image style={{
-                    position: "absolute",
-                    top: "1250px",
-                    left: "920px",
-                }
-                }
-                    src="/Group 10.png"
-                    width={120}
-                    height={95}
-                    alt=""
-                />
-            </div>
-
-        </div>
-
-    )
-
-}
+export const SecondSection = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        backgroundColor: "#F5F7FA",
+        padding: "100px 150px",
+        fontSize: "48px",
+        gap: "20px",
+      }}
+    >
+      <div style={{ padding: "100px 130px", gap: "30px" }}>
+        <TitleAndDescription
+          h1label="Your Hub for teamwork"
+          h2label="Give everyone you work with—inside and outside your company—a more productive way to stay in sync. Respond faster with emoji, keep conversations focused in channels, and simplify all your communication into one place."
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          backgroundColor: "#F5F7FA",
+          alignItems: "center",
+          fontSize: "48px",
+        }}
+      >
+        <Image
+          style={{
+            border: "1px",
+          }}
+          src="/first-pic.png"
+          width={500}
+          height={500}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "./components/button";
-import { EmailInput } from "./components/email-input";
 import Image1 from "../public/image1.jpg";
 import { BlueButton } from "./components/blue-button";
-import { NavigationItems } from "./components/navigation-items"
-import { TitleAndDescription } from "./components/title-and-description";
-import Image from "next/image";
-import { Star } from "./components/star";
+import { Button } from "./components/button";
+import { EmailInput } from "./components/email-input";
+import { NavigationItems } from "./components/navigation-items";
 import { SecondSection } from "./components/second-section";
-import { ThirdSection } from "./components/third-section";
+import { Star } from "./components/star";
+import { TitleAndDescription } from "./components/title-and-description";
+
 export default function Home() {
   return (
     <main>
@@ -20,19 +19,18 @@ export default function Home() {
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           color: "white",
-          backgroundColor: "#F5F7FA"
-
+          backgroundColor: "#F5F7FA",
         }}
       >
-        <div style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          gap: "30px",
-          padding: "10px",
-          fontSize: "18px",
-        }
-        }>
-
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            gap: "30px",
+            padding: "10px",
+            fontSize: "18px",
+          }}
+        >
           <NavigationItems>Product</NavigationItems>
           <NavigationItems>Service</NavigationItems>
           <NavigationItems>Contacts</NavigationItems>
@@ -51,30 +49,27 @@ export default function Home() {
             top: "252px",
             paddingLeft: "124px",
             paddingRight: "120px",
-
           }}
         >
-
           <TitleAndDescription
             h1label="Instant collaborations for remote teams"
             h2label="All in one for your remote team chats, collaboration and track projects"
           />
 
-          <div style={{
-            display: "flex",
-            marginTop: "55px",
-            gap: "18px",
-            color: "white",
-          }}
+          <div
+            style={{
+              display: "flex",
+              marginTop: "55px",
+              gap: "18px",
+              color: "white",
+            }}
           >
             <EmailInput />
             <BlueButton />
-            
-            <SecondSection />
-            <ThirdSection />
           </div>
         </div>
       </div>
+      <SecondSection />
       <div>
         <p
           style={{
@@ -88,15 +83,8 @@ export default function Home() {
           What people say about us
         </p>
 
-
         <Star />
       </div>
-
-
-
-    </main >
-
+    </main>
   );
 }
-
-
